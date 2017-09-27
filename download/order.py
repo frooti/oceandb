92f7bb6997bd2a73bdc9a2def09ebafeb0cad763
ravi <ravi@dataraft.in>
2017-09-27 23:02:31 +0530

    fixed bugs

--- download/order.py
+++ download/order.py
@@ -9,14 +9,14 @@ for o in orders:
 		datapoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
 		for d in datapoints:
 			try:
-				data.append(d.loc.coordinates[0], d.loc.coordinates[1], d.height)
+				data.append(d.loc['coordinates'][0], d.loc['coordinates'][1], d.height)
 			except Exception, e:
 				print e
 	elif o.data == 'bathymetry':	
 		datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
 		for d in datapoints:
 			try:
-				data.append(d.loc.coordinates[0], d.loc.coordinates[1], d.depth)
+				data.append(d.loc['coordinates'][0], d.loc['coordinates'][1], d.depth)
 			except Exception, e:
 				print e
 
