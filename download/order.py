b9eb22c2096801efd81f920724016adad470b8b8
ravi <ravi@dataraft.in>
2017-09-27 23:01:21 +0530

    fixed bugs

--- download/order.py
+++ download/order.py
@@ -10,14 +10,14 @@ for o in orders:
 		for d in datapoints:
 			try:
 				data.append(d.loc.coordinates[0], d.loc.coordinates[1], d.height)
-			except:
-				pass
+			except Exception, e:
+				print e
 	elif o.data == 'bathymetry':	
 		datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
 		for d in datapoints:
 			try:
 				data.append(d.loc.coordinates[0], d.loc.coordinates[1], d.depth)
-			except:
-				pass
+			except Exception, e:
+				print e
 
 	print data
