53d23905b9c68dfb303b26cc3e3a2cc4bc797ab8
ravi <ravi@dataraft.in>
2017-09-27 23:19:12 +0530

    updating order.py

--- download/order.py
+++ download/order.py
@@ -1,23 +1,30 @@
 from models import *
+import csv
 
 orders = order.objects(download_link=None)
 for o in orders:
-	data = []
-	datapoints = []
+	with open('tmp.csv', 'w') as f:
+		datapoints = []
 
-	if o.data == 'wave':
-		datapoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
-		for d in datapoints:
-			try:
-				data.append([d.loc['coordinates'][0], d.loc['coordinates'][1], d.height])
-			except Exception, e:
-				print e
-	elif o.data == 'bathymetry':	
-		datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
-		for d in datapoints:
-			try:
-				data.append([d.loc['coordinates'][0], d.loc['coordinates'][1], d.depth])
-			except Exception, e:
-				print e
+		if o.data == 'wave':
+			fieldnames = ['long', 'lat', 'height']
+    		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    		writer.writeheader()
 
-	print data
+			datapoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
+			for d in datapoints:
+				try:
+					writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'height': d.height})
+				except Exception, e:
+					print e
+		elif o.data == 'bathymetry':
+			fieldnames = ['long', 'lat', 'depth']
+    		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    		writer.writeheader()
+
+			datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
+			for d in datapoints:
+				try:
+					writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'depth': d.depth})
+				except Exception, e:
+					print e
