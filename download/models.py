62344c9af18684dd8a546e8ede8cb62967404537
ravi <ravi@dataraft.in>
2017-09-27 12:09:45 +0530

    added data models.

--- download/models.py
+++ download/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+from mongoengine import *
+
+connect('ocean')
+
+class wave(Document):
+	loc = PointField(db_field='l', auto_index=True, required=True)
+	height = FloatField(db_field='h', required=True)
+
+class bathymetry(Document):
+	loc = PointField(db_field='l', auto_index=True, required=True)
+	depth = FloatField(db_field='d', required=True)
