2a669ed791ef2c612416c73a13319a5a372c85c7
ravi <ravi@dataraft.in>
2017-09-28 00:17:49 +0530

    updating order.py

--- download/order.py
+++ download/order.py
@@ -1,9 +1,26 @@
 from models import *
 import csv
+import boto3
+s3 = boto3.resource('s3')
+
+def upload_file(filename, oid):
+	session = boto3.Session()
+	s3_client = session.client('s3')
+
+	try:
+		print "Uploading file:", filename
+
+		tc = boto3.s3.transfer.TransferConfig()
+		t = boto3.s3.transfer.S3Transfer(client=s3_client, config=tc)
+
+		t.upload_file(filename, 'dataraftoceandb', oid)
+
+	except Exception as e:
+		print "Error uploading: %s" % ( e )
 
 orders = order.objects(download_link=None)
 for o in orders:
-	with open('tmp.csv', 'w') as f:
+	with open('tmp.csv', 'r+') as f:
 		datapoints = []
 
 		if o.data == 'wave':
@@ -28,3 +45,6 @@ for o in orders:
 					writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'depth': d.depth})
 				except Exception, e:
 					print e
+
+		# publish to s3
+		
