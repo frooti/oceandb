97f94575519a9d2308c248aa9984bcd963a62b61
ravi <ravi@dataraft.in>
2017-09-27 17:01:01 +0530

    added order api

--- download/models.py
+++ download/models.py
@@ -6,8 +6,22 @@ from django.db import models
 # Create your models here.
 
 from mongoengine import *
+from datetime import datetime
 
 connect('ocean')
+
+class order(Document):
+	oid = StringField(db_field='oid', max_length=50, required=True)
+	email = StringField(db_field='e', max_length=200, required=True)
+	organization = StringField(db_field='o', max_length=200, required=True)
+	polygon = PolygonField(db_field='p', auto_index=True, required=True)
+	data = StringField(db_field='d', max_length=50, required=True)
+	price = DecimalField(db_field='p', precision=2, rounding='ROUND_HALF_UP', required=True) 
+	payment_status = BooleanField(db_field='ps', default=False)
+	downloadlink = StringField(db_field='dl', default=None)
+	email_sent = BooleanField(db_field='em', default=False)
+	created_at = DateTimeField(db_field='ct', default=datetime.now())
+	processed_at = DateTimeField(db_field='pt', default=None)
 
 class wave(Document):
 	loc = PointField(db_field='l', auto_index=True, required=True)
--- download/views.py
+++ download/views.py
@@ -2,5 +2,86 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import order, wave, bathymetry
 
 # Create your views here.
+DEFAULT_RESPONSE = '{"status":false, "msg": "bad request."}'
+
+from django.http import HttpResponse
+import boto3
+import redis
+import json
+import uuid
+import time
+from decimal import Decimal
+
+# utilities
+def default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError
+
+def fetchPrice(request):
+	res = json.loads(DEFAULT_RESPONSE)
+	polygon = request.GET.get('polygon', None)
+	dtype =  request.GET.get('dtype', None)
+	
+	datapoints = 0
+	try:
+		polygon = json.loads(polygon)
+		if dtype and polygon:
+			if dtype == 'wave':
+				datapoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry': polygon}}}).count()
+			elif dtype == 'bathymetry':
+				datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry': polygon}}}).count()
+		res['status'] = True
+		res['price'] = '$'+str(datapoints*0.1)
+		res['datapoints'] = datapoints
+		res['size'] = str(datapoints*20/1024)+' KB'
+	except Exception, e:
+		print e
+		res['status'] = False
+		res['msg'] = 'Someting went wrong.'
+
+	return HttpResponse(json.dumps(res, default=default))
+
+def getPrice(data, polygon):
+	datapoints = 0
+	if data and polygon:
+		try:
+			if data=='wave':
+				datapoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry': polygon}}}).count()
+				return datapoints*0.1, datapoints
+			elif dtype == 'bathymetry':
+				datapoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry': polygon}}}).count()
+				return datapoints*0.1, datapoints
+		except:
+			pass
+	return -1, 0
+
+def fetchData(request):
+	res = json.loads(DEFAULT_RESPONSE)
+	polygon = request.GET.get('polygon', None)
+	data =  request.GET.get('data', None)
+	email = request.GET.get('email', None)
+	organization = request.GET.get('organization', None)
+
+	try:
+		polygon = json.loads(polygon)
+		if data and polygon and email and organization:
+			o = order(oid=str(uuid.uuid4()))
+			o.data = data
+			o.polygon = polygon
+			o.organization = organization
+			o.email = email
+			o.price = getPrice(data, polygon)
+			o.save()
+
+		res['status'] = True
+		res['msg'] = 'order placed.'
+	except Exception, e:
+		print e
+		res['status'] = False
+		res['msg'] = 'Someting went wrong.'
+
+	return HttpResponse(json.dumps(res, default=default))
