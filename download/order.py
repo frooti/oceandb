2d77482a4a2c9ee68494c8ef94e40faf6caa7002
ravi <ravi@dataraft.in>
2017-10-01 18:31:36 +0530

    added email msg

--- download/order.py
+++ download/order.py
@@ -2,7 +2,9 @@ from models import *
 import csv
 from datetime import datetime, timedelta
 import boto3
-s3 = boto3.resource('s3')
+import time
+
+ses = boto3.client('ses')
 
 def upload_file(filename, oid):
 	session = boto3.Session()
@@ -19,47 +21,66 @@ def upload_file(filename, oid):
 	except Exception as e:
 		print "Error uploading: %s" % ( e )
 
-orders = order.objects(processed_at=None)
-for o in orders:
-	with open('/home/ubuntu/projects/oceandb/download/tmp.csv', 'w') as f:
-		datapoints = []
+while True:
+	try:
+		orders = order.objects(processed_at=None)
+		for o in orders:
+			with open('/home/ubuntu/projects/oceandb/download/tmp.csv', 'w') as f:
+				datapoints = []
 
-		if o.data == 'wave':
-			from_date = o.from_date
-			to_date = o.to_date
-			fieldnames = ['long', 'lat', 'height', 'date']
-			writer = csv.DictWriter(f, fieldnames=fieldnames)
-			writer.writeheader()
-
-			spatialpoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
-			for d in spatialpoints:
-				try:
-					values = d.values
+				if o.data == 'wave':
 					from_date = o.from_date
-					
-					while from_date<=to_date:
-						day = str(from_date.timetuple().tm_yday)
-						year = str(from_date.year)
+					to_date = o.to_date
+					fieldnames = ['long', 'lat', 'height', 'date']
+					writer = csv.DictWriter(f, fieldnames=fieldnames)
+					writer.writeheader()
+
+					spatialpoints = wave.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
+					for d in spatialpoints:
 						try:
-							writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'height': d.values[year][day], 'date':from_date.strftime('%Y-%m-%d %H:%M')})
+							values = d.values
+							from_date = o.from_date
+
+							while from_date<=to_date:
+								day = str(from_date.timetuple().tm_yday)
+								year = str(from_date.year)
+								try:
+									writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'height': d.values[year][day], 'date':from_date.strftime('%Y-%m-%d %H:%M')})
+								except Exception, e:
+									print e 
+								from_date += timedelta(days=1)
 						except Exception, e:
-							print e 
-						from_date += timedelta(days=1)
-				except Exception, e:
-					print e
-		elif o.data == 'bathymetry':
-			fieldnames = ['long', 'lat', 'depth']
-			writer = csv.DictWriter(f, fieldnames=fieldnames)
-			writer.writeheader()
+							print e
+				elif o.data == 'bathymetry':
+					fieldnames = ['long', 'lat', 'depth']
+					writer = csv.DictWriter(f, fieldnames=fieldnames)
+					writer.writeheader()
 
-			spatialpoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
-			for d in spatialpoints:
-				try:
-					writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'depth': d.depth})
-				except Exception, e:
-					print e
-		f.close()
+					spatialpoints = bathymetry.objects(__raw__={'l':{'$geoWithin':{'$geometry':o.polygon}}})
+					for d in spatialpoints:
+						try:
+							writer.writerow({'long': d.loc['coordinates'][0], 'lat': d.loc['coordinates'][1], 'depth': d.depth})
+						except Exception, e:
+							print e
+				f.close()
 
-		# publish to s3
-		upload_file('/home/ubuntu/projects/oceandb/download/tmp.csv', o.oid)
-		print 'https://s3-ap-southeast-1.amazonaws.com/dataraftoceandb/'+o.oid
+				# publish to s3
+				upload_file('/home/ubuntu/projects/oceandb/download/tmp.csv', o.oid)
+				download_link = 'https://s3-ap-southeast-1.amazonaws.com/dataraftoceandb/'+o.oid
+				
+				# email client
+				email_msg = 'Hi, \n Below is your order #'+o.oid+' download link:\n'+download_link
+				email = {
+					Source = 'order@dataraft.in',
+					Destination = {'ToAddresses': o.email},
+					Message = {'Subject': {'Data': 'reg: Order #'+str(o.oid), ''}},
+					Body = {'Text': {'Data': email_msg}}
+				}
+				ses.send_email(**email)
+
+				o.processed_at = datetime.now()
+				o.download_link = download_link
+				o.save()
+		except Exception, e:
+			print e
+	time.sleep(5)
