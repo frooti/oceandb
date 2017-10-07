52a425b67d0ee9cd0aa4afe24fd99bef2a9e3167
ravi <ravi@dataraft.in>
2017-10-07 22:48:50 +0530

    added session_engine

--- oceandb/session_engine.py
+++ oceandb/session_engine.py
@@ -23,86 +23,86 @@ class Session(Document):
 
 class SessionStore(SessionBase):
 	"""
-    Implements database session store.
-    """
-    def __init__(self, session_key=None):
-        super(SessionStore, self).__init__(session_key)
+	Implements database session store.
+	"""
+	def __init__(self, session_key=None):
+		super(SessionStore, self).__init__(session_key)
 
-    @classmethod
-    def get_model_class(cls):
+	@classmethod
+	def get_model_class(cls):
 		return Session
 
-    @cached_property
-    def model(self):
-        return self.get_model_class()
+	@cached_property
+	def model(self):
+		return self.get_model_class()
 
-    def load(self):
-        try:
-            s = self.model.objects(session_key=self.session_key, expire_date__gt=datetime.now()).first()
-            return self.decode(s.session_data)
-        except Exception as e:
-            self._session_key = None
-            return {}
+	def load(self):
+		try:
+			s = self.model.objects(session_key=self.session_key, expire_date__gt=datetime.now()).first()
+			return self.decode(s.session_data)
+		except Exception as e:
+			self._session_key = None
+			return {}
 
 	def exists(self, session_key):
-        if self.model.objects(session_key=session_key).first()
-        	return True
-    	else:
-    		return False
+		if self.model.objects(session_key=session_key).first()
+			return True
+		else:
+			return False
 
-    def create(self):
-        while True:
-            self._session_key = self._get_new_session_key()
-            try:
-                # Save immediately to ensure we have a unique entry in the
-                # database.
-                self.save(must_create=True)
-            except CreateError:
-                # Key wasn't unique. Try again.
-                continue
-            self.modified = True
-            return
+	def create(self):
+		while True:
+			self._session_key = self._get_new_session_key()
+			try:
+				# Save immediately to ensure we have a unique entry in the
+				# database.
+				self.save(must_create=True)
+			except CreateError:
+				# Key wasn't unique. Try again.
+				continue
+			self.modified = True
+			return
 
-    def create_model_instance(self, data):
-        """
-        Return a new instance of the session model object, which represents the
-        current session state. Intended to be used for saving the session data
-        to the database.
-        """
-        return self.model(
-            session_key=self._get_or_create_session_key(),
-            session_data=self.encode(data),
-            expire_date=self.get_expiry_date(),
-        )
+	def create_model_instance(self, data):
+		"""
+		Return a new instance of the session model object, which represents the
+		current session state. Intended to be used for saving the session data
+		to the database.
+		"""
+		return self.model(
+			session_key=self._get_or_create_session_key(),
+			session_data=self.encode(data),
+			expire_date=self.get_expiry_date(),
+		)
 
-    def save(self, must_create=False):
-        """
-        Saves the current session data to the database. If 'must_create' is
-        True, a database error will be raised if the saving operation doesn't
-        create a *new* entry (as opposed to possibly updating an existing
-        entry).
-        """
-        if self.session_key is None:
-            return self.create()
-        data = self._get_session(no_load=must_create)
-        obj = self.create_model_instance(data)
+	def save(self, must_create=False):
+		"""
+		Saves the current session data to the database. If 'must_create' is
+		True, a database error will be raised if the saving operation doesn't
+		create a *new* entry (as opposed to possibly updating an existing
+		entry).
+		"""
+		if self.session_key is None:
+			return self.create()
+		data = self._get_session(no_load=must_create)
+		obj = self.create_model_instance(data)
 
-        try:
-        	obj.save()
-        except:
-        	raise
+		try:
+			obj.save()
+		except:
+			raise
 
-    def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        try:
-            self.model.objects(session_key=session_key).delete()
-        except:
-            pass
+	def delete(self, session_key=None):
+		if session_key is None:
+			if self.session_key is None:
+				return
+			session_key = self.session_key
+		try:
+			self.model.objects(session_key=session_key).delete()
+		except:
+			pass
 
-    @classmethod
-    def clear_expired(cls):
-        cls.get_model_class().objects(expire_date__lt=datetime.now()).delete()
+	@classmethod
+	def clear_expired(cls):
+		cls.get_model_class().objects(expire_date__lt=datetime.now()).delete()
 
