d7a161642beb8cd7d3bc3fadc421e0e3cb7c3fbe
ravi <ravi@dataraft.in>
2017-12-06 14:30:41 +0530

    added chull.py

---
+++ download/chull.py
@@ -0,0 +1,23 @@
+from scipy.spatial import ConvexHull
+import csv
+
+file_path = '/Users/ravi/Desktop/bathy_zones/Cochin_iocl.xyz'
+
+points = []
+chull  = []
+
+with open(file_path, 'rb') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+	for i, row in enumerate(spamreader):
+		if i==0:
+			continue
+		elif row:
+			points.append([float(row[1]), float(row[0])])
+
+	hull = ConvexHull(points)
+
+	for i in hull.vertices:
+		chull.append(points[i])
+
+	chull = chull+[chull[0]]
+	print chull
