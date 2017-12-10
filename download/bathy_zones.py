fe82817a08bc25caf2fe8ff0975a813555493392
ravi <ravi@dataraft.in>
2017-12-11 00:29:25 +0530

    added bathy_zones.py

---
+++ download/bathy_zones.py
@@ -0,0 +1,42 @@
+import sys
+from os import listdir
+from os.path import isfile, join
+sys.path.append('/home/dataraft/projects/oceandb')
+sys.stdout.flush()
+
+from datetime import datetime, timedelta
+from mongoengine import *
+from download.models import bathymetry
+
+connect('ocean', host='mongodb://localhost:27017/ocean')
+
+## CONFIG ##
+directory = '/tmp/bathy_zones'
+FILES = [f for f in listdir(directory) if isfile(join(directory, f))]
+## CONFIG ##
+
+for file_path in FILES:
+	print file_path
+	
+	with open(file_path, 'r') as f:
+	data = []
+	for i, r in enumerate(f):
+		if i==0:
+			continue
+		elif i and i%1000==0:
+			bathymetry.objects.insert(data)
+			data = []
+			print i
+		else:
+			values = r.split('\t')
+			longitute = float(values[1])
+			latitude = float(values[0])
+			depth = float(values[2])
+
+			loc = {'type': 'Point', 'coordinates': [longitute, latitude]}
+			data.append(bathymetry(loc=loc, depth=depth))
+	else:
+		bathymetry.objects.insert(data)
+		data = []
+		print 'completed!'
+
