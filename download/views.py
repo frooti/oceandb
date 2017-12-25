eb7b497e45f4b97f044e42f3af3baa6cd02e5998
ravi <ravi@dataraft.in>
2017-12-26 01:57:30 +0530

    triangulation testing

--- download/views.py
+++ download/views.py
@@ -162,7 +162,7 @@ def getZone(request):
 				if z.name=='Ennore':
 					bathymetry.append({'zid':z.zid, 'name':z.name, 'polygon':z.polygon, 'triangles': z.triangles})
 				else:
-					bathymetry.append({'zid':z.zid, 'name':z.name, 'polygon':z.polygon})
+					bathymetry.append({'zid':z.zid, 'name':z.name, 'polygon':z.polygon, 'triangles': z.triangles})
 		res['zones'] = zones
 		res['bathymetry'] = bathymetry
 		res['status'] = True
