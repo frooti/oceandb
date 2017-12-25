82b7d96823616145bbcaf111f865b35f4e87c19d
ravi <ravi@dataraft.in>
2017-12-26 01:38:51 +0530

    triangulation testing

--- download/triangulate.py
+++ download/triangulate.py
@@ -39,17 +39,29 @@ for z in zone.objects(ztype='bathymetry'):
 	 
 	for t in tri['triangles']:
 		t = [list(tri['vertices'][i]) for i in t]
-		t = [[int(i[0]*100000.0)/100000.0, int(i[1]*100000.0)/100000.0] for i in t]
-		t = t+[t[0]]
-		pipeline = [
-			{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
-			{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-		]
-		value = 0
-		q = list(bathymetry.objects.aggregate(*pipeline))
-		if q:
-			value = round(q[0].get('depth', 0), 2)
-		data.append([t, value])
+		rt = [[round(i[0], 5), round(i[1], 5)] for i in t]
+		try:
+			pipeline = [
+				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt+[rt[0]]]}}}} },
+				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+			]
+			value = 0
+			q = list(bathymetry.objects.aggregate(*pipeline))
+			if q:
+				value = round(q[0].get('depth', 0), 2)
+			data.append([t, value])
+		except Exception, e:
+			print e
+			pipeline = [
+				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t+[t[0]]]}}}} },
+				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+			]
+			value = 0
+			q = list(bathymetry.objects.aggregate(*pipeline))
+			if q:
+				value = round(q[0].get('depth', 0), 2)
+			data.append([t, value])
+
 	
 	z.triangles = data
 	z.save()
