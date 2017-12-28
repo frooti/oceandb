f2ee4360019186f86c578ded8f0f35c60ae5e150
ravi <ravi@dataraft.in>
2017-12-28 15:32:32 +0530

    testing triangulation.

--- download/triangulate.py
+++ download/triangulate.py
@@ -2,68 +2,72 @@ import sys
 sys.path.append('/home/dataraft/projects/oceandb')
 sys.stdout.flush()
 
-import triangle
+import triangle # mesh generator
+from shapely.ops import triangulate # Delauny
+from shapely.geometry import Point
+from shapely.geometry import shape
+from shapely.geometry import mapping
 import numpy as np
 from download.models import zone, userzone, bathymetry, userbathymetry
 
 # user zones
 for uz in userzone.objects():
 	data = []
-	polygon = {'vertices': uz.polygon['coordinates'][0]}
-	polygon['vertices'] = np.array(polygon['vertices'])
-	
-	tri = triangle.triangulate(polygon, opts='q25D')
-	 
-	for t in tri['triangles']:
-		t = [list(tri['vertices'][i]) for i in t]
-		t = t+[t[0]]
+	points = [shape(p) for p in userbathymetry.objects(uzid=uz.uzid)]
+	triangles = triangulate(points)
+
+	for t in triangles:
+		t = mapping(t.wkt)
+		
 		pipeline = [
-			{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
+			{ "$match": {'l': {'$geoIntersects': t}} },
 			{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
 		]
 		value = 0
-		q = list(bathymetry.objects.aggregate(*pipeline))
+		q = list(userbathymetry.objects.aggregate(*pipeline))
 		if q:
-			value = round(q[0].get('depth', 0), 2)
+			value = round(q[0].get('depth', 0), 1)
+		print [t['coordinates'], value]
 
-	uz.triangles = data
-	uz.save()
+
+	#uz.triangles = data
+	#uz.save()
 
 # public zones
-for z in zone.objects(ztype='bathymetry'):
-	data = []
-	polygon = {'vertices': z.polygon['coordinates'][0]}
-	polygon['vertices'] = np.array(polygon['vertices'])
+# for z in zone.objects(ztype='bathymetry'):
+# 	data = []
+# 	polygon = {'vertices': z.polygon['coordinates'][0]}
+# 	polygon['vertices'] = np.array(polygon['vertices'])
 	
-	tri = triangle.triangulate(polygon, opts='q25D')
+# 	tri = triangle.triangulate(polygon, opts='q25D')
 	 
-	for t in tri['triangles']:
-		t = [list(tri['vertices'][i]) for i in t]
-		rt = [[round(i[0], 5), round(i[1], 5)] for i in t]
-		rt = rt+[rt[0]]
-		try:
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([rt, value])
-		except Exception, e:
-			print e
-			t = t+[t[0]]
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([t, value])
+# 	for t in tri['triangles']:
+# 		t = [list(tri['vertices'][i]) for i in t]
+# 		rt = [[round(i[0], 5), round(i[1], 5)] for i in t]
+# 		rt = rt+[rt[0]]
+# 		try:
+# 			pipeline = [
+# 				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
+# 				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+# 			]
+# 			value = 0
+# 			q = list(bathymetry.objects.aggregate(*pipeline))
+# 			if q:
+# 				value = round(q[0].get('depth', 0), 2)
+# 			data.append([rt, value])
+# 		except Exception, e:
+# 			print e
+# 			t = t+[t[0]]
+# 			pipeline = [
+# 				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
+# 				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+# 			]
+# 			value = 0
+# 			q = list(bathymetry.objects.aggregate(*pipeline))
+# 			if q:
+# 				value = round(q[0].get('depth', 0), 2)
+# 			data.append([t, value])
 
 	
-	z.triangles = data
-	z.save()
+# 	z.triangles = data
+# 	z.save()
