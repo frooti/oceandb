9f80b426942fe85d27ccf96f1978c2135a126e6d
ravi <ravi@dataraft.in>
2017-12-28 22:18:23 +0530

    testing triangulation.

--- download/triangulate.py
+++ download/triangulate.py
@@ -8,34 +8,51 @@ from download.models import zone, userzone, bathymetry, userbathymetry
 
 # user zones
 for uz in userzone.objects():
+	print uz.uzid
 	data = []
 	polygon = {'vertices': uz.polygon['coordinates'][0]}
 	polygon['vertices'] = np.array(polygon['vertices'])
 	
-	tri = triangle.triangulate(polygon, opts='q25D')
+	tri = triangle.triangulate(polygon, opts='a.0125')
 	 
 	for t in tri['triangles']:
 		t = [list(tri['vertices'][i]) for i in t]
-		t = t+[t[0]]
-		pipeline = [
-			{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
-			{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-		]
-		value = 0
-		q = list(bathymetry.objects.aggregate(*pipeline))
-		if q:
-			value = round(q[0].get('depth', 0), 2)
+		rt = [[round(i[0], 5), round(i[1], 5)] for i in t]
+		rt = rt+[rt[0]]
+		try:
+			pipeline = [
+				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
+				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+			]
+			value = 0
+			q = list(bathymetry.objects.aggregate(*pipeline))
+			if q:
+				value = round(q[0].get('depth', 0), 2)
+			data.append([rt, value])
+		except Exception, e:
+			print e
+			t = t+[t[0]]
+			pipeline = [
+				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
+				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+			]
+			value = 0
+			q = list(bathymetry.objects.aggregate(*pipeline))
+			if q:
+				value = round(q[0].get('depth', 0), 2)
+			data.append([t, value])
 
 	uz.triangles = data
 	uz.save()
 
 # public zones
 for z in zone.objects(ztype='bathymetry'):
+	print z.zid
 	data = []
 	polygon = {'vertices': z.polygon['coordinates'][0]}
 	polygon['vertices'] = np.array(polygon['vertices'])
 	
-	tri = triangle.triangulate(polygon, opts='q25D')
+	tri = triangle.triangulate(polygon, opts='a.0125')
 	 
 	for t in tri['triangles']:
 		t = [list(tri['vertices'][i]) for i in t]
