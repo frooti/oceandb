cf20631e60ad152f19405fc71a853c1b677dbc6a
ravi <ravi@dataraft.in>
2018-01-02 15:42:09 +0530

    updated meshing lib

--- download/triangulate.py
+++ download/triangulate.py
@@ -2,9 +2,31 @@ import sys
 sys.path.append('/home/dataraft/projects/oceandb')
 sys.stdout.flush()
 
+from meshpy.triangle import MeshInfo, build
 import triangle
+import math
 import numpy as np
 from download.models import zone, userzone, bathymetry, userbathymetry
+
+dmax = 10.0e-04
+def interpolate_polygon(polygon): # geojson
+	vertices = []
+	for i in range(0, len(polygon['coordinates'][0])-1): # for each edge
+		v1 = [round(polygon['coordinates'][0][i][0], 6), round(polygon['coordinates'][0][i][1], 6)]
+		v2 = [round(polygon['coordinates'][0][i+1][0], 6), round(polygon['coordinates'][0][i+1][1], 6)]
+		di = math.hypot(v2[1]-v1[1], v2[0]-v1[0])
+		ui = [(v2[0]-v1[0])/di, (v2[1]-v1[1])/di]
+		
+
+		if di>dmax: # interpolate
+			vertices.append(v1)
+			for j in range(0, int(di/dmax)):
+				vi = [round(v1[0]+(((j+1)*dmax)*ui[0]), 6), round(v1[1]+(((j+1)*dmax)*ui[1]), 6)]
+				vertices.append(vi)
+		else:
+			vertices.append(v1)
+		
+	return = vertices+[vertices[0]]
 
 # user zones
 for uz in userzone.objects():
@@ -49,13 +71,17 @@ for uz in userzone.objects():
 for z in zone.objects(ztype='bathymetry'):
 	print z.zid
 	data = []
-	polygon = {'vertices': z.polygon['coordinates'][0]}
-	polygon['vertices'] = np.array(polygon['vertices'])
+	mesh_info = MeshInfo()
+	interpolated_chull = interpolate_polygon(z.polygon)
+
+	mesh_info = MeshInfo()
+	mesh_info.set_points(interpolated_chull['coordinates'][0])
+	facets = [(i, i+1) for i in range(0, len(interpolated_chull)-1)]
+	mesh_info.set_facets(facets)
+	mesh = build(mesh_info)
 	
-	tri = triangle.triangulate(polygon, opts='a.0125')
-	 
-	for t in tri['triangles']:
-		t = [list(tri['vertices'][i]) for i in t]
+	for t in mesh.elements:
+		t = [mesh.points[i] for i in t]
 		rt = [[round(i[0], 5), round(i[1], 5)] for i in t]
 		rt = rt+[rt[0]]
 		try:
