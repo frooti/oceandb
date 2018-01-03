1a220ed170ca9d0f1649cb099359941a1a931374
ravi <ravi@dataraft.in>
2018-01-03 21:13:03 +0530

    testing triangulation

--- download/triangulate.py
+++ download/triangulate.py
@@ -29,61 +29,59 @@ def interpolate_polygon(polygon): # geojson
 	return vertices+[vertices[0]]
 
 # user zones
-for uz in userzone.objects():
-	print uz.uzid
-	data = []
-	interpolated_chull = interpolate_polygon(uz.polygon)
-	interpolated_chull = [[int(v[0]*1e8), int(v[1]*1e8)] for v in interpolated_chull]
-	polygon = {'vertices': interpolated_chull}
-	polygon['vertices'] = np.array(polygon['vertices'])
+# for uz in userzone.objects():
+# 	print uz.uzid
+# 	data = []
+# 	interpolated_chull = interpolate_polygon(uz.polygon)
+# 	interpolated_chull = [[int(v[0]*1e8), int(v[1]*1e8)] for v in interpolated_chull]
+# 	polygon = {'vertices': interpolated_chull}
+# 	polygon['vertices'] = np.array(polygon['vertices'])
 	
-	tri = triangle.triangulate(polygon, opts='q')
+# 	tri = triangle.triangulate(polygon, opts='q')
 	 
-	for t in tri['triangles']:
-		t = [list(tri['vertices'][i]) for i in t]
-		t = [[i[0]/1.0e8, i[1]/1.0e8] for i in t]
-		rt = [[round(i[0], 8), round(i[1], 8)] for i in t]
-		rt = rt+[rt[0]]
-		try:
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([rt, value])
-		except Exception, e:
-			#print e
-			t = t+[t[0]]
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([t, value])
+# 	for t in tri['triangles']:
+# 		t = [list(tri['vertices'][i]) for i in t]
+# 		t = [[i[0]/1.0e8, i[1]/1.0e8] for i in t]
+# 		rt = [[round(i[0], 8), round(i[1], 8)] for i in t]
+# 		rt = rt+[rt[0]]
+# 		try:
+# 			pipeline = [
+# 				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
+# 				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+# 			]
+# 			value = 0
+# 			q = list(bathymetry.objects.aggregate(*pipeline))
+# 			if q:
+# 				value = round(q[0].get('depth', 0), 2)
+# 			data.append([rt, value])
+# 		except Exception, e:
+# 			#print e
+# 			t = t+[t[0]]
+# 			pipeline = [
+# 				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
+# 				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+# 			]
+# 			value = 0
+# 			q = list(bathymetry.objects.aggregate(*pipeline))
+# 			if q:
+# 				value = round(q[0].get('depth', 0), 2)
+# 			data.append([t, value])
 
-	uz.triangles = data
-	uz.save()
+# 	uz.triangles = data
+# 	uz.save()
 
 # public zones
 for z in zone.objects(ztype='bathymetry'):
 	print z.zid
 	data = []
-	interpolated_chull = interpolate_polygon(z.polygon)
-	interpolated_chull = [[int(v[0]*1e8), int(v[1]*1e8)] for v in interpolated_chull]
-	polygon = {'vertices': interpolated_chull}
-	polygon['vertices'] = np.array(polygon['vertices'])
+	mesh_info = MeshInfo()
+	mesh_info.set_points(z.polygon['coordinates'][0])
+	facets = [(i, i+1) for i in range(0, len(z.polygon['coordinates'][0])-1)]
+	mesh_info.set_facets(facets)
+	mesh = build(mesh_info)
 
-	tri = triangle.triangulate(polygon, opts='q')
-	
-	for t in tri['triangles']:
+	for t in mesh.elements:
 		t = [list(tri['vertices'][i]) for i in t]
-		t = [[i[0]/1.0e8, i[1]/1.0e8] for i in t]
 		rt = [[round(i[0], 8), round(i[1], 8)] for i in t]
 		rt = rt+[rt[0]]
 		try:
