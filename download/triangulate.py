3bae93834a9950ba2083293b3234313bc4a3a2fe
ravi <ravi@dataraft.in>
2018-01-04 18:25:55 +0530

    testing triangulation

--- download/triangulate.py
+++ download/triangulate.py
@@ -2,9 +2,8 @@ import sys
 sys.path.append('/home/dataraft/projects/oceandb')
 sys.stdout.flush()
 
+import subprocess
 from shapely.geometry import Polygon
-from meshpy.triangle import MeshInfo, build
-import triangle
 import math
 import numpy as np
 from download.models import zone, userzone, bathymetry, userbathymetry
@@ -28,6 +27,36 @@ def interpolate_polygon(polygon): # geojson
 			vertices.append(v1)
 		
 	return vertices+[vertices[0]]
+
+def tri_input(polygon):
+	text = str(len(polygon))+' 2 0 0\n\n' # header
+	for i, v in enumerate(polygon):
+		text += str(i+1)+' '+str(v[0])+' '+str(v[1])+'\n'
+	return text
+
+def tri_get_vertices(file):
+	vertices = []
+	f = open(file, 'r')
+	for i, l in enumerate(f):
+		if i==0:
+			continue
+		else:
+			v = l.split()
+			if v[0]!='#':
+				vertices.append([float(v[1]), float(v[2])])
+	return vertices
+
+def tri_get_triangles(file):
+	triangles = []
+	f = open(file, 'r')
+	for i, l in enumerate(f):
+		if i==0:
+			continue
+		else:
+			v = l.split()
+			if v[0]!='#':
+				triangles.append([float(v[1]), float(v[2]), float(v[3])])
+	return triangles
 
 def transform_polygon(polygon, origin, reverse=False):
 	if reverse:
@@ -81,42 +110,52 @@ for uz in userzone.objects():
 for z in zone.objects(ztype='bathymetry'):
 	print z.zid
 	data = []
-	mesh_info = MeshInfo()
 	origin = z.polygon['coordinates'][0][0]
 	p = transform_polygon(z.polygon['coordinates'][0], origin=origin)
-	mesh_info.set_points(p)
-	facets = [(i, i+1) for i in range(0, len(p)-1)]
-	mesh_info.set_facets(facets)
-	max_volume = int(Polygon(p).area/200)
-	mesh = build(mesh_info, max_volume=max_volume)
+	tri_input = tri_input(p)
+	
+	f = open('/tmp/triangle/'+z.zid+'.node', 'w')
+	f.write(tri_input)
+	f.close()
+	
+	max_area = int(Polygon(p).area/200)
+	out_bytes = subprocess.check_output(['triangle', '-a'+str(max_area), '/var/triangle/'+str(z.zid)+'.node'])
+	output = out_bytes.decode('utf-8')
 
-	for t in mesh.elements:
-		t = transform_polygon([mesh.points[i] for i in t], origin=origin, reverse=True)
-		rt = [[round(i[0], 6), round(i[1], 6)] for i in t]
-		rt = rt+[rt[0]]
-		try:
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([rt, value])
-		except Exception, e:
-			#print e
-			t = t+[t[0]]
-			pipeline = [
-				{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
-				{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
-			]
-			value = 0
-			q = list(bathymetry.objects.aggregate(*pipeline))
-			if q:
-				value = round(q[0].get('depth', 0), 2)
-			data.append([t, value])
+	if 'Writing '+str(z.zid)+'.1.node' and 'Writing '+str(z.zid)+'.1.ele':
+		vertices = tri_get_vertices(str(z.zid)+'.1.node')
+		triangles = tri_get_triangles(str(z.zid)+'.1.ele')
+
+		print 'vertices: '+str(vertices)
+		print 'triangles: '+str(triangles)
+
+	# for t in mesh.elements:
+	# 	t = transform_polygon([mesh.points[i] for i in t], origin=origin, reverse=True)
+	# 	rt = [[round(i[0], 6), round(i[1], 6)] for i in t]
+	# 	rt = rt+[rt[0]]
+	# 	try:
+	# 		pipeline = [
+	# 			{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [rt]}}}} },
+	# 			{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+	# 		]
+	# 		value = 0
+	# 		q = list(bathymetry.objects.aggregate(*pipeline))
+	# 		if q:
+	# 			value = round(q[0].get('depth', 0), 2)
+	# 		data.append([rt, value])
+	# 	except Exception, e:
+	# 		#print e
+	# 		t = t+[t[0]]
+	# 		pipeline = [
+	# 			{ "$match": {'l': {'$geoIntersects': {'$geometry': {'type': 'Polygon', 'coordinates': [t]}}}} },
+	# 			{ "$group": {"_id": None, "depth": { "$avg": "$d" }} },
+	# 		]
+	# 		value = 0
+	# 		q = list(bathymetry.objects.aggregate(*pipeline))
+	# 		if q:
+	# 			value = round(q[0].get('depth', 0), 2)
+	# 		data.append([t, value])
 
 	
-	z.triangles = data
-	z.save()
+	# z.triangles = data
+	# z.save()
