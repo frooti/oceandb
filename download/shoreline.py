f9de4b1167c3643f4f3e1060039bca029ae2ddc4
ravi <ravi@dataraft.in>
2018-01-11 14:09:11 +0530

    added shoreline.py

---
+++ download/shoreline.py
@@ -0,0 +1,33 @@
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+sys.path.append('/home/dataraft/projects/oceandb')
+sys.stdout.flush()
+
+from cStringIO import StringIO
+from xml.dom.minidom import parseString
+from zipfile import ZipFile
+import math
+
+def openKMZ(filename):
+    zip=ZipFile(filename)
+    for z in zip.filelist:
+        if z.filename[-4:] == '.kml':
+            fstring=zip.read(z)
+            break
+    else:
+        raise Exception("Could not find kml file in %s" % filename)
+    return fstring
+
+def openKML(filename):
+    try:
+        fstring=openKMZ(filename)
+    except Exception:
+        fstring=open(filename,'r').read()
+    return parseString(fstring)
+
+xml = openKML(filename)
+linestring = xml.getElementsByTagName('Linestring')[0]
+coordinates = linestring.getElementsByTagName('coordinates')[0]
+coordinates = coordinates.strip().split(' ')
