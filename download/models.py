eebf6e9f1977f92a7fc669bc9613c4f1dc90a179
ravi <ravi@dataraft.in>
2018-01-16 22:30:14 +0530

    updated username password

--- download/models.py
+++ download/models.py
@@ -8,7 +8,8 @@ from django.db import models
 from mongoengine import *
 from datetime import datetime
 
-connect('ocean', host='mongodb://localhost:27017/ocean', username='ocean', password='@cean99')
+db = connect('ocean', host='mongodb://localhost:27017/ocean', username='ocean', password='@cean99')
+db["ocean"].authenticate("ocean", password="@cean99")
 
 class zone(Document):
 	zid = StringField(db_field='zid', max_length=50, required=True)
