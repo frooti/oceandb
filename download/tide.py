ee50b42770b8789e1ce3a41543f4b071d8f0f500
ravi <ravi@dataraft.in>
2018-01-17 17:52:05 +0530

    added tide.py

---
+++ download/tide.py
@@ -0,0 +1,29 @@
+import sys 
+sys.stdout.flush()
+
+import scipy.io
+
+
+from datetime import datetime, timedelta
+import pymongo
+CONN = pymongo.MongoClient('localhost', 27017, username='ocean', password='@cean99', authMechanism='MONGODB-CR')
+DB = CONN['ocean']
+TIDE = DB.tide
+
+## CONFIG ##
+file_path = 'tide.mat'
+grid = (720, 1046)
+## CONFIG ##
+
+mat = scipy.io.loadmat(file_path)
+lng = mat['data'][0][0]
+lat = mat['data'][0][1]
+val = mat['data'][0][4]
+
+i, j = 0
+
+while i<grid[0]:
+	while j<grid[1]:
+		print lng[i][j], lat[i][j]
+		j = j+1
+	i = i+1
