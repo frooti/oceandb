dfb0b5020a94a498cdc4843734241f556a726cd9
ravi <ravi@dataraft.in>
2018-01-17 19:30:30 +0530

    testing tide.

--- download/tide.py
+++ download/tide.py
@@ -21,12 +21,19 @@ lat = mat['data']['Y'][0][0]
 val = mat['data']['Val'][0][0]
 
 i, j = 0, 0
-points = []
 
 while i<grid[0]:
+	print i
+	bulk = TIDE.initialize_unordered_bulk_op()
+	
 	while j<grid[1]:
-		points.append([lng[i][j], lat[i][j]])
-		j = j+1
+		try:
+			loc = {'type': 'Point', 'coordinates': [round(float(lng[i][j]), 3), round(float(lat[i][j]]), 3)}
+			height = float('1.0')
+			bulk.find({'l':{'$geoIntersects': {'$geometry': loc}}}).upsert().update({'$set': {'l': loc, 'height': height}})
+			j = j+1
+		except:
+			pass
+	bulk.execute() # batch update
 	i = i+1
 	j = 0
-print len(points)
