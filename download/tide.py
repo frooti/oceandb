35ff7903037181b7fa28b59770b6457998499aca
ravi <ravi@dataraft.in>
2018-01-18 20:11:59 +0530

    updated tide.py

--- download/tide.py
+++ download/tide.py
@@ -13,30 +13,37 @@ TIDE = DB.tide
 ## CONFIG ##
 file_path = '/tmp/tide.mat'
 grid = (720, 1046)
+date = datetime(day=1, month=5, year=2017) #GMT
+timestep = timedelta(hours=12)
 ## CONFIG ##
 
-mat = scipy.io.loadmat(file_path)
-lng = mat['data']['X'][0][0]
-lat = mat['data']['Y'][0][0]
-val = mat['data']['Val'][0][0]
+MAT = scipy.io.loadmat(file_path)
+LNG = MAT['data']['X'][0][0]
+LAT = MAT['data']['Y'][0][0]
+VAL = MAT['data']['Val'][0][0]
+TIMESTEPS = len(VAL)
 
-i, j = 0, 0
+for t in range(0, TIMESTEPS):
+	print 'TIMESTEP: '+t
+	day = str(date.timetuple().tm_yday)
+	hour = int(date.hour)
+	date += timestep
 
-for i in range(0, grid[0]):
-	print i
-	bulk = TIDE.initialize_unordered_bulk_op()
-	
-	for j in range(0, grid[1]):
+	for i in range(0, grid[0]):
+		print i
+		bulk = TIDE.initialize_unordered_bulk_op()
+		
+		for j in range(0, grid[1]):
+			try:
+				longitude = round(float(lng[i][j]), 3)
+				latitude = round(float(lat[i][j]), 3)
+				value = float(VAL[t][i][j])
+				if not isnan(longitude) and  not isnan(latitude) and not isnan(value):
+					loc = {'type': 'Point', 'coordinates': [longitude, latitude]}
+					bulk.find({'l':{'$geoIntersects': {'$geometry': loc}}}).upsert().update({'$set': {'l': loc, 'values.'+day+'.'+hour: value}})
+			except Exception, e:
+				print e
 		try:
-			longitude = round(float(lng[i][j]), 3)
-			latitude = round(float(lat[i][j]), 3)
-			value = float('1.0')
-			if not isnan(longitude) and  not isnan(latitude) and not isnan(value):
-				loc = {'type': 'Point', 'coordinates': [longitude, latitude]}
-				bulk.find({'l':{'$geoIntersects': {'$geometry': loc}}}).upsert().update({'$set': {'l': loc, 'h': value}})
+			bulk.execute() # batch update
 		except Exception, e:
 			print e
-	try:
-		bulk.execute() # batch update
-	except Exception, e:
-		print e
