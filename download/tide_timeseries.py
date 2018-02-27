b8790286c1ff37945c69f5d692d3e56c2ba924ec
dataraft <hello@dataraft.in>
2018-02-27 17:27:52 +0530

    updated file path

--- download/tide_timeseries.py
+++ download/tide_timeseries.py
@@ -26,7 +26,7 @@ def timeseries():
 	global date
 	global START
 
-	for f in sorted(glob.glob(file_path), key=lambda x: int(x.split('/')[-1].split('_')[-1])):
+	for f in sorted(glob.glob(file_path), key=lambda x: int(x.split('/')[-1].split('_')[-1].split('.')[0])):
 		print 'PROCESSING: '+str(f)
 		MAT = scipy.io.loadmat(f)
 		LNG = MAT['data']['X'][0][0]
