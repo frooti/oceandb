6a391e4f753c0cd354f883a0b325134ace057bdb
ravi <ravi@dataraft.in>
2018-03-04 00:39:49 +0530

    monthly visualisation

--- download/views.py
+++ download/views.py
@@ -188,7 +188,7 @@ def getZoneData(request):
 			z = zone.objects(zid=zid).first()
 			tri = z.triangles
 
-			if z.type=='zone' and month:
+			if z.ztype=='zone' and month:
 				for i in range(len(tri)):
 					for j in range(len(tri[i])):
 						if isinstance(tri[i][j], dict):
