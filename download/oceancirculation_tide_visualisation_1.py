cec4f30cba4bac7e59ec87fe60b06a330802257f
ravi <ravi@dataraft.in>
2018-03-21 12:54:50 +0530

    added tide_visualisation.

--- download/oceancirculation_tide_visualisation_1.py
+++ download/oceancirculation_tide_visualisation_1.py
@@ -5,6 +5,7 @@ import glob
 import json
 import gc
 import numpy as np
+import utm
 
 from math import isnan
 
@@ -15,6 +16,7 @@ file_path = '/var/www/dataraft.in/1_jan_tide.mat'
 output_path = 'oceancirculatin_tide_visualisation.data'
 date = datetime(day=1, month=1, year=2018) # GMT
 timestep = timedelta(days=0, hours=2, minutes=0)
+UTM_ZONE = 45 
 NODE_COUNT = 68604
 ELEMENT_COUNT = 134659
 ## CONFIG ##
@@ -40,9 +42,8 @@ def visualisation():
 					try:
 						line = [float(i) for i in line]
 						node += 1
-						lng = line[5]
-						lat = line[6]
-						depth = line[1] 
+						lng, lat = utm.to_latlon(line[0], line[1], UTM_ZONE, 'U')
+						depth = line[2]
 						NODES[node] = [lng, lat, depth]
 					except Exception, e:
 						print e
