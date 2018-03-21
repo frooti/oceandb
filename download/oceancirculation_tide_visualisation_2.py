284a8504ea304834b77b2f50a13c199d43e32f6a
ravi <ravi@dataraft.in>
2018-03-21 17:00:09 +0530

    added tide_visualisation_2.py

---
+++ download/oceancirculation_tide_visualisation_2.py
@@ -0,0 +1,45 @@
+import os
+import sys 
+sys.stdout.flush()
+import glob
+import json
+import gc
+
+from datetime import datetime, timedelta
+import dateutil.parser
+import pymongo
+CONN = pymongo.MongoClient("mongodb://ocean:%40cean99@10.24.1.151/ocean?authSource=ocean")
+DB = CONN['ocean']
+TIDE_VIS = DB.tide_visualisation
+
+## CONFIG ##
+zid = 'test-project'
+file_path = 'oceancirculation_tide_visualisation.data'
+## CONFIG ##
+
+START = datetime.now()
+
+def upload():
+	with open(file_path) as f:
+		bulk = TIDE_VIS.initialize_unordered_bulk_op()
+
+		objects = []
+		for i, line in enumerate(f):
+			print i
+			line = line.strip().split('$$')
+			polygon = json.loads(line[0])
+			depth = float(line[1])
+			date = dateutil.parser.parse(line[2])
+
+			objects.append({'zid':zid, 'p':polygon, 'd':depth, 'dt':date})
+			
+			if i%1000==0:
+				bulk.insert_many(objects) # batch insert
+				objects = []
+
+		bulk.insert_many(objects) # left over
+		print 'completed!'
+		print 'TIME: '+str(datetime.now()-START)
+
+if __name__ == '__main__':
+	upload()
