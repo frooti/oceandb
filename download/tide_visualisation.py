5af4a2ac2f506fa1c0ee797345284197b9c5e896
ravi <ravi@dataraft.in>
2018-04-09 16:55:26 +0530

    added tide_visualisation.py

---
+++ download/tide_visualisation.py
@@ -0,0 +1,43 @@
+import scipy.io
+import shapely.geometry import asShape, mapping
+from statistics import mean
+from download.models import zone, tide, tide_visualisation
+from datetime import datetime, timedelta
+
+## CONFIG ##
+grid_file = 'current/uv/1.5_jan_current.mat'
+## CONFIG ##
+
+MAT = scipy.io.loadmat(grid_file)
+LNG = MAT['data']['X'][0][0]
+LAT = MAT['data']['Y'][0][0]
+GRID = MAT['data']['X'][0][0].shape
+
+def visualisation():
+	elements = []
+	for i in GRID[0]:
+		for j in GRID[1]:
+			if i!=GRID[0]-1 and j!=GRID[1]-1:
+				p1 = [round(float(LNG[i][j]), 3), round(float(LAT[i][j]), 3)]
+				p2 = [round(float(LNG[i][j+1]), 3), round(float(LAT[i][j+1]), 3)]
+				p3 = [round(float(LNG[i+1][j+1]), 3), round(float(LAT[i+1][j+1]]), 3)]
+				p4 = [round(float(LNG[i+1][j]), 3), round(float(LAT[i+1][j]]), 3)]
+				elements.append(asShape({'type': 'Polygon', 'coordinates': [[p1, p2, p3, p4, p1]]}))
+
+	for z in zone.objects(zid='b951a954-f3f7-44f6-80a6-0194cbee50a1'):
+		zpolygon = shape(z.polygon)
+		for e in elements:
+			if e.intersects(zpolygon):
+				points = [t for t in tide.objects(loc__geo_intersects=mapping(e))]
+				values = []
+				for p in points:
+					values.append(p['values']['1']['0'])
+
+				tv = tidevisualisation(zid=z.zid)
+				tv.date = datetime(year=2018, month=1, day=1)
+				tv.polygon = mapping(e)
+				tv.depth = mean(values)
+				tv.save()
+
+if __name__ == '__main__':
+	visualisation()
