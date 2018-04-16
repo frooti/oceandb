8e88b7559d1fd3a7bfc4cfa68fd7363122631a1f
ravi <ravi@dataraft.in>
2018-04-16 23:05:04 +0530

    updated current_visualisation

--- download/current_visualisation.py
+++ download/current_visualisation.py
@@ -1,3 +1,7 @@
+import sys 
+sys.path.append('/home/dataraft/projects/oceandb')
+sys.stdout.flush()
+
 import scipy.io
 from shapely.geometry import asShape, mapping
 from statistics import mean
@@ -5,7 +9,7 @@ from download.models import zone, current, currentdirection, current_visualisati
 from datetime import datetime, timedelta
 
 ## CONFIG ##
-grid_file = 'current/uv/1.5_jan_current.mat'
+grid_file = '/rawdata/current/1.5_jan_current.mat'
 ## CONFIG ##
 
 MAT = scipy.io.loadmat(grid_file)
@@ -18,23 +22,41 @@ def visualisation():
 	for i in GRID[0]:
 		for j in GRID[1]:
 			p = [round(float(LNG[i][j]), 3), round(float(LAT[i][j]), 3)]
-			elements.append(asShape({'type': 'Point', 'coordinates': p}))
+			try:
+				point = asShape({'type': 'Point', 'coordinates': p})
+				if point.is_valid:
+					elements.append(point)
+			except:
+				pass
 
 	for z in zone.objects(zid='b951a954-f3f7-44f6-80a6-0194cbee50a1'):
+		print 'PROCESSING: '+str(z.zid)+' '+str(z.name)
 		zpolygon = shape(z.polygon)
 		for e in elements:
-			if e.intersects(zpolygon):
+			if zpolygon.intersects(e):
+				print mapping(e)
 				point = current.objects(loc__geo_intersects=mapping(e)).first()
 				point2 = currentdirection.objects(loc__geo_intersects=mapping(e)).first()
 
 				if point and point2:
+					data = []
 					for i in range(1,366):
+						if data:
+							current_visualisation.objects.insert(data)
+							data = []
 						cv = current_visualisation(zid=z.zid)
 						cv.date = datetime(year=2018, month=1, day=i)
 						cv.loc = mapping(e)
 						cv.speed = point['values'][str(i)]['0']
 						cv.direction = point2['values'][str(i)]['0']
-						cv.save()
+						data.append(cv)
+					else:
+						if data:
+							current_visualisation.objects.insert(data)
+							data = []
 
 if __name__ == '__main__':
+	START = datetime.now()
 	visualisation()
+	print 'TIME: '+str(datetime.now()-START)
+	print 'completed!'
